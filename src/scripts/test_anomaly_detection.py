49b58b374cbefbf3a7f822188776f69f89557fc2
Le0p <50958675+Le0p@users.noreply.github.com>
2024-12-11 20:47:35 +0100

    Update test_anomaly_detection.py

--- src/scripts/test_anomaly_detection.py
+++ src/scripts/test_anomaly_detection.py
@@ -12,8 +12,6 @@ import pickle
 from sklearn.metrics import roc_auc_score, precision_score, recall_score, f1_score, accuracy_score, roc_curve
 import matplotlib.pyplot as plt
 
-from alpiq.data.sequence_dataset import SequenceDataset
-from alpiq.model.causal_model import CausalModel
 from alpiq.evaluation.anomaly_detection import compute_score, windowed_threshold_batch
 
 
@@ -29,7 +27,7 @@ class SimpleDataset(torch.utils.data.Dataset):
         return {"outputs": self.outputs_list[idx], "targets": self.targets_list[idx]}
 
 
-@hydra.main(config_path="configs", config_name="test_model_VG5_anom_02c")
+@hydra.main(config_path="configs", config_name="test_model_VG6_anom_01a")
 def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
 
@@ -58,22 +56,19 @@ def main(cfg: DictConfig):
     if cfg.data.load_GT:
         GT_list = np.array(GT_list)
 
+    # Load the mean and std from the evaluation dataset
+    mean_std_file = cfg.data.file_mean_std
+    with open(mean_std_file, 'rb') as f:
+        mean_std = pickle.load(f)
+    mean = mean_std[0]
+    std = mean_std[1]
+
+    # Compute thresholds based on the mean and std
+    thresholds = mean + cfg.testing.threshold * std
+
     # Create a simple data loader to iterate over outputs_list, targets_list and GT_list
     prerun_dataset = SimpleDataset(outputs_list, targets_list)
-
-    # Load datasets and create data loaders
-#    test_dataset = SequenceDataset(
- #       data_path=Path(cfg.data.test_file),
-  #      context_length=cfg.model.context_length,
-   #     input_feature_col_names=cfg.data.input_feature_cols,
-    #    current_value_col_names=cfg.data.current_value_cols,
-     #   next_value_col_names=cfg.data.next_value_cols,
-      #  padding_value=cfg.data.padding_value,
-       # load_GT=cfg.data.load_GT
-    #)
-
     prerun_loader = DataLoader(prerun_dataset, batch_size=cfg.testing.batch_size, shuffle=False)
-    #test_loader = DataLoader(test_dataset, batch_size=cfg.testing.batch_size, shuffle=False)
 
 
     outputs_buffer = []
@@ -83,10 +78,6 @@ def main(cfg: DictConfig):
 
     scores_list = []
     anomalies_list = []
-
-    ope_mod_list = []
-    #for batch in tqdm(test_loader):
-     #   ope_mod_list.extend(batch["operating_mode"].numpy())
 
 
     buffer_size = cfg.testing.buffer_size
@@ -113,7 +104,7 @@ def main(cfg: DictConfig):
 
         # Compute the anomaly state
         if len(scores_buffer) > cfg.testing.window_size:
-            states_buffer = windowed_threshold_batch(scores_buffer, cfg.testing.threshold, cfg.testing.window_size, states_buffer, cfg.testing.nb_consecutive_anomalies, batch_size)
+            states_buffer = windowed_threshold_batch(scores_buffer, thresholds, cfg.testing.window_size, states_buffer, cfg.testing.nb_consecutive_anomalies, batch_size)
 
             # Check if any of the new states are anomalies
             new_states = [state[-batch_size:] for state in states_buffer]
@@ -121,38 +112,65 @@ def main(cfg: DictConfig):
             anomalies_list.extend(anomalies_batch)
 
     print(f"Anomalies shape: {len(anomalies_list)}")
-    print(f"GT shape: {len(GT_list)}")
-
     if cfg.data.load_GT:
-        # Create a figure with n+2 subplots, n being the number of cols in cfg.data.next_value_cols,
-        # so in each plot you will the output and the target and the score for each of the cols,
-        # in the n+1 plot you will see the total score
-        # and in the last plot you will see the anomalies and the GT
-        t_plot = 40000
-        t_max = min(t_plot, len(outputs_list))
-        n = len(cfg.data.next_value_cols)
-        fig, axs = plt.subplots(n+2, 1, figsize=(15, (n+1)*5))
-        for i in range(n):
-            axs[i].plot([outputs_list[j][i] for j in range(t_max)], label='Outputs')
-            axs[i].plot([targets_list[j][i] for j in range(t_max)], label='Targets')
-            # axs[i].plot([scores_list[j][i] for j in range(t_max)], label='Scores')
-            axs[i].legend()
-            axs[i].set_title(cfg.data.next_value_cols[i])
+        print(f"GT shape: {len(GT_list)}")
 
-        axs[n].plot([scores_list[j][-1] for j in range(t_max)], label='Total score')
-        axs[n].set_title('Total score')
 
-        axs[n+1].plot(anomalies_list[:t_max], label='Anomalies')
+    # Create a figure with n+2 subplots, n being the number of cols in cfg.data.next_value_cols,
+    # so in each plot you will the output and the target and the score for each of the cols,
+    # in the n+1 plot you will see the total score
+    # and in the last plot you will see the anomalies and the GT
+    t_plot = 25000
+    t_max = min(t_plot, len(outputs_list))
+    n = len(cfg.data.next_value_cols)
+    fig, axs = plt.subplots(n+2, 1, figsize=(15, (n+1)*5))
+    for i in range(n):
+        axs[i].plot([outputs_list[j][i] for j in range(t_max)], label='Outputs')
+        axs[i].plot([targets_list[j][i] for j in range(t_max)], label='Targets')
+        axs[i].plot([scores_list[j][i] for j in range(t_max)], label='Scores')
+        # print the threshold
+        axs[i].axhline(y=thresholds[i], color='r', linestyle='--', label='Threshold')
+        axs[i].legend()
+        axs[i].set_title(cfg.data.next_value_cols[i])
+
+    axs[n].plot([scores_list[j][-1] for j in range(t_max)], label='Total score')
+    axs[n].axhline(y=thresholds[-1], color='r', linestyle='--', label='Threshold')
+    axs[n].set_title('Total score')
+
+    axs[n+1].plot(anomalies_list[:t_max], label='Anomalies')
+    if cfg.data.load_GT:
         axs[n+1].plot(GT_list[:t_max], label='GT')
-        #axs[n+1].plot(ope_mod_list[:t_max], label='Operating Mode')
-        axs[n+1].legend()
-        axs[n+1].set_title('Anomalies and GT')
+    #axs[n+1].plot(ope_mod_list[:t_max], label='Operating Mode')
+    axs[n+1].legend()
+    axs[n+1].set_title('Anomalies and GT')
 
-        # save the figure
-        fig_file = os.path.join(cfg.testing.results_dir, cfg.testing.fig_file)
+    # save the figure
+    fig_file = os.path.join(
+        cfg.testing.results_dir,
+        f"{cfg.testing.fig_file}_th{cfg.testing.threshold}_win{cfg.testing.window_size}_cons{cfg.testing.nb_consecutive_anomalies}.png"
+    )
+    plt.savefig(fig_file)
+    plt.show()
+
+    for i in range(n):
+        plt.figure(figsize=(15, 5))
+        plt.plot([outputs_list[j][i] for j in range(t_max)], label='Outputs')
+        plt.plot([targets_list[j][i] for j in range(t_max)], label='Targets')
+        plt.plot([scores_list[j][i] for j in range(t_max)], label='Scores')
+        plt.axhline(y=thresholds[i], color='r', linestyle='--', label='Threshold')
+        plt.legend()
+        plt.xlabel('Timesteps')
+        plt.ylabel('Normalized Values')
+        plt.title(f"Prediction of {cfg.data.next_value_cols[i]}")
+        fig_file = os.path.join(
+            cfg.testing.results_dir,
+            f"{cfg.testing.fig_file}_{cfg.data.next_value_cols[i]}.png"
+        )
         plt.savefig(fig_file)
         plt.show()
 
+
+    if cfg.data.load_GT:
         # Find the indices where the ground truth is 1
         GT_indices = np.where(GT_list == 1)[0]
         # Find continuous sequences of indices
@@ -173,18 +191,26 @@ def main(cfg: DictConfig):
         for i in range(n):
             axs[i].plot([outputs_list[j][i] for j in range(seq2show[0], seq2show[1])], label='Outputs')
             axs[i].plot([targets_list[j][i] for j in range(seq2show[0], seq2show[1])], label='Targets')
-            # axs[i].plot([scores_list[j][i] for j in range(seq2show[0], seq2show[1])], label='Scores')
+            # smooth the scores with sliding windows
+            scores_list_smoothed = [np.mean(scores_list[j][i:i+cfg.testing.window_size]) for j in range(seq2show[0], seq2show[1])]
+            axs[i].plot(scores_list_smoothed, label='Scores')
+            axs[i].axhline(y=thresholds[i], color='r', linestyle='--', label='Threshold')
             axs[i].legend()
             axs[i].set_title(cfg.data.next_value_cols[i])
 
         axs[n].plot([scores_list[j][-1] for j in range(seq2show[0], seq2show[1])], label='Total score')
+        axs[n].axhline(y=thresholds[-1], color='r', linestyle='--', label='Threshold')
         axs[n].set_title('Total score')
 
         axs[n+1].plot(anomalies_list[seq2show[0]:seq2show[1]], label='Anomalies')
         axs[n+1].plot(GT_list[seq2show[0]:seq2show[1]], label='GT')
-        #axs[n+1].plot(ope_mod_list[seq2show[0]:seq2show[1]], label='Operating Mode')
         axs[n+1].legend()
         axs[n+1].set_title('Anomalies and GT')
+        fig_file_2 = os.path.join(
+            cfg.testing.results_dir,
+            f"{cfg.testing.fig_file}_continuous_seq_th{cfg.testing.threshold}_win{cfg.testing.window_size}_cons{cfg.testing.nb_consecutive_anomalies}.png"
+        )
+        plt.savefig(fig_file_2)
         plt.show()
 
 
